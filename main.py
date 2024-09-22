0375e69d39d0aebba79aa73074c6cc2317f09830
Dương Nguyễn <duonglifetech@gmail.com>
2024-09-23 00:54:19 +0700

    Update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 from io import BytesIO
 from flask import Flask, request
 from telegram import Bot
+import asyncio
 
 API_TOKEN = os.getenv('API_TOKEN')
 WEBHOOK_URL = os.getenv('WEBHOOK_URL')  # URL webhook của bạn
@@ -60,8 +61,6 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_document(document=output, filename='processed_image.png')
 
 # Hàm xử lý lệnh hello
-# async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     await update.message.reply_text(f'Hello {update.effective_user.first_name}! Hãy gửi cho tôi một bức ảnh hoặc một tệp ảnh để tôi chèn logo vào.')
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     print("Received /hello command")
     await update.message.reply_text(f'Hello {update.effective_user.first_name}!')
@@ -80,15 +79,15 @@ application.add_handler(MessageHandler(filters.Document.IMAGE, handle_document))
 @app_flask.route(f'/{API_TOKEN}', methods=['POST'])
 def webhook():
     update = Update.de_json(request.get_json(), application.bot)
-    application.update_queue.put(update)
+    asyncio.run_coroutine_threadsafe(application.update_queue.put(update), application.loop)
     return "OK", 200
 
 if __name__ == '__main__':
     bot = Bot(token=API_TOKEN)
-    
+
     # Chạy setup_webhook trong một vòng lặp sự kiện
-    import asyncio
     loop = asyncio.get_event_loop()
     loop.run_until_complete(setup_webhook(bot))
-    
+
+    # Khởi động Flask
     app_flask.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
